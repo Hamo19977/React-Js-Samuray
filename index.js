6d2956b74d914294ab75aa5e8ba3b67dcc55a406
Hamo <develop@Hamos-MacBook-Air.local>
2022-12-03 23:24:09 +0400

    1-6 lessons

---
+++ index.js
@@ -0,0 +1,46 @@
+
+// const  CC = require('currency-converter-lt');
+//
+//
+// let currencyConverter = new CC({from: 'USD', to: "AMD", amount: 1000})
+//
+//
+// currencyConverter.convert().then((response) => {
+//     console.log(`Result  ${response}`)
+//
+// })
+
+
+
+// const  os = require('os');
+// let res = os.platform()
+//  console.log(res)
+//
+//
+//  const my_math = require('./my_math');
+//
+// let res1 = my_math.add(1,9);
+// let res2 = my_math.minus(21,11);
+//  console.log(`add ${res1}  minus ${res2}`)
+
+
+
+// const  fs = require('fs');
+// fs.readFile('some.txt', 'utf-8', (error,data) => {
+//     fs.writeFile('some.txt', 'Hello Word\n' + data, (err,res) => {
+//         console.log('resultt:' + data )
+//     });
+// })
+// console.log(`resuult: ${result}`)
+
+
+// const fs = require('fs')
+// fs.mkdirSync('text-files')
+
+// fs.mkdir('text-files', () => {
+//     fs.writeFile('./text-files/some.txt', 'Hello', () => {});
+// })
+
+// fs.unlink('./text-files/some.txt', () => {
+//     fs.rmdir('./text-files', () =>{})
+// })
---
+++ my_math.js
@@ -0,0 +1,10 @@
+
+ /// for lesson two
+
+// const add = (a,b) => a + b;
+// const minus = (a,b) => a - b;
+//
+// module.exports = {
+//     add: add,
+//     minus: minus
+// }
